24f4603580f433f8df00f36fc0b7d803e074784d
sachineedigara <147805678+sachineedigara@users.noreply.github.com>
2023-10-13 13:33:10 +0530

    Create demo.py

---
+++ demo.py
@@ -0,0 +1,17 @@
+def sort(nums):
+    for i in range(5):
+        minpos = i
+        for j in range(i,6):
+            if nums[j]<nums[minpos]:
+                minpos=j
+
+        temp =nums[i]
+        nums[i]=nums[minpos]
+        nums[minpos]=temp
+
+        print(nums)                 # to see how its swaping
+
+nums = [5,7,9,3,4,6]
+sort(nums)
+
+print(nums)
